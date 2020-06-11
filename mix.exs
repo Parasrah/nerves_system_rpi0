d6fc10f43814f202640710cbfeb9e482319621d4
parasrah <git@parasrah.com>
2020-06-10 23:31:33 -0600

    adding workflows from rpi3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi0.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
+  @github_organization "parasrah"
   @app :nerves_system_rpi0
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
